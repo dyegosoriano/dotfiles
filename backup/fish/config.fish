90114ca213fa4c1f1bfe3b53f75e765193f44ab5
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-02-06 00:14:13 -0300

    Update backup files

--- backup/fish/config.fish
+++ backup/fish/config.fish
@@ -10,3 +10,13 @@ set -x PATH $HOME/.asdf/shims $PATH
 
 # Inicializar asdf
 source ~/.asdf/asdf.fish
+
+# Inportando alias
+if test -f ~/.bash_aliases
+    for line in (cat ~/.bash_aliases | grep -E "^alias " | sed -E "s/^alias ([a-zA-Z0-9_-]+)='(.*)'/\1 \2/")
+        set alias_name (echo $line | cut -d ' ' -f1)
+        set alias_cmd (echo $line | cut -d ' ' -f2-)
+        abbr -a $alias_name $alias_cmd
+    end
+end
+
