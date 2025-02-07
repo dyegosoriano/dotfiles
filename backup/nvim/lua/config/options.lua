f99377a44793034fa170b4999151a3ad8f4c4431
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-02-07 19:41:38 -0300

    adicionando configurações de fontes

--- backup/nvim/lua/config/options.lua
+++ backup/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.guifont = "Fira Code"
+vim.opt.relativenumber = false
+vim.opt.number = true
+
+vim.g.netrw_list_hide = ''
