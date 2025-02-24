975746fcd255de524f6e7f6ab301176ee01d7cfc
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-02-24 19:49:12 -0300

    feat: inicializando configurações do telescope

---
+++ backup/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    defaults = {
+      file_ignore_patterns = { "node_modules/", "%.git/", "dist/", "%.jpeg", "%.jpg", "%.png", "%.jpg", "%.pdf" },
+      layout_strategy = "horizontal",
+    },
+  },
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Buscar Arquivos" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Buffers Abertos" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Buscar Texto" },
+  },
+}
