c540c1120e4b2adad090cc247c8688fb4acb78d4
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-02-25 18:09:16 -0300

    feat: configurando asdf no fish

--- backup/fish/config.fish
+++ backup/fish/config.fish
@@ -5,20 +5,17 @@ end
 starship init fish | source # Inicializando o Starship
 set -g fish_greeting "" # Removendo a saudação do Fish
 
-# ASDF configuration code
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
+if test -d ~/.asdf # ASDF configuration
+    source ~/.asdf/asdf.fish
+
+    if not contains $_asdf_shims $PATH
+        set -gx --prepend PATH $_asdf_shims
+    end
+
+    set --erase _asdf_shims
 end
 
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
-
-# Importando alias
-if test -f ~/.bash_aliases
+if test -f ~/.bash_aliases # Importando alias
     for line in (cat ~/.bash_aliases | grep -E "^alias " | sed -E "s/^alias ([a-zA-Z0-9_-]+)='(.*)'/\1 \2/")
         set alias_name (echo $line | cut -d ' ' -f1)
         set alias_cmd (echo $line | cut -d ' ' -f2-)
