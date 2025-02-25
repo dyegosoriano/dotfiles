044b8ab3516f1e32bc2750e40b2e9a31da331f25
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-02-24 21:37:53 -0300

    feat: configurando esquema de cores

---
+++ backup/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      styles = { sidebars = "transparent", floats = "transparent" },
+      -- transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "tokyonight" },
+  },
+}
