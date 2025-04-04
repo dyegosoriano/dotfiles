6e22016ae4fd71b16bc1b11fc8f3523c37ab1a8f
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-04-04 19:21:15 -0300

    refactor: shortcut key adjustments

--- backup/nvim/lua/config/keymaps.lua
+++ backup/nvim/lua/config/keymaps.lua
@@ -1,6 +1,4 @@
--- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 
 local function map(mode, lhs, rhs, opts)
   local keys = require("lazy.core.handler").handlers.keys
@@ -32,5 +30,16 @@ map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Buscar por arq
 map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Buscar por texto" })
 
 -- Neotree
-map("n", "<leader>er", "<cmd>Neotree filesystem reveal right<cr>", { desc = "" })
-map("n", "<leader>el", "<cmd>Neotree filesystem reveal left<cr>", { desc = "" })
+map("n", "<leader>e>", "<cmd>Neotree filesystem reveal right<cr>", { desc = "" })
+map("n", "<leader>e<", "<cmd>Neotree filesystem reveal left<cr>", { desc = "" })
+
+-- Lazygit
+if vim.fn.executable("lazygit") == 1 then
+  map("n", "<leader>gg", function() Snacks.lazygit( { cwd = LazyVim.root.git() }) end, { desc = "Lazygit (Root Dir)" })
+  map("n", "<leader>gl", function() Snacks.picker.git_log({ cwd = LazyVim.root.git() }) end, { desc = "Git Log" })
+  map("n", "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Current File History" })
+  map("n", "<leader>gL", function() Snacks.picker.git_log() end, { desc = "Git Log (cwd)" })
+  map("n", "<leader>gG", function() Snacks.lazygit() end, { desc = "Lazygit (cwd)" })
+end
+
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" }) -- quit
