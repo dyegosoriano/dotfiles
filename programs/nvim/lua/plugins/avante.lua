380e0c5739232f27585a999b00e15a510fd33ffd
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-04-27 20:12:05 -0300

    feat: configurando assistente de IA para neovim

---
+++ programs/nvim/lua/plugins/avante.lua
@@ -0,0 +1,52 @@
+-- https://github.com/yetone/avante.nvim
+-- if true then return {} end
+
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false,
+  opts = {
+    provider = "openai",
+    openai = {
+      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      endpoint = "https://api.openai.com/v1",
+      max_completion_tokens = 4096,
+      model = "gpt-4o",
+      temperature = 0,
+      timeout = 30000,
+    },
+  },
+  build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+
+    -- --- The below dependencies are optional,
+    -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          drag_and_drop = { insert_mode = true },
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = { file_types = { "markdown", "Avante" } },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
