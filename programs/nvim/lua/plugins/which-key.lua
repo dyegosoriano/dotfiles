279b3abf1860efcfae6c535359a94e5ae1a99dd0
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-05-23 21:03:42 -0300

    feat: adicionando plugin de exibição das teclas de atalhos

---
+++ programs/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,28 @@
+-- https://github.com/folke/which-key.nvim_set_hl
+-- if true then return {} end
+
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "classic", -- tipo de layout (classic, modern, helix)
+    win = {
+      title_pos = "center",
+      title = true,
+    },
+    layout = {
+      height = { min = 4, max = 10 },
+      width = { min = 20, max = 50 },
+      spacing = 3,
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ buffer = 0 })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
