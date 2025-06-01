7150ad363c6dc64d58601c6641ee2c7b42d39e9c
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-06-01 15:15:07 -0300

    feat: create backup script

--- scripts/sync-files/backup-files.sh
+++ scripts/sync-files/backup-files.sh
@@ -9,5 +9,12 @@ SRC="/home/soriano"
 clear
 
 echo -e '\n\033[0;36mCopy files...\033[0m\n'
-rsync -azv -e ssh --update --include=* $SRC $USER@$HOST:$DEST --chown=soriano:soriano
+
+rsync -azv --delete -e ssh --update \
+--exclude='node_modules' \
+--exclude='.config/*' \
+--exclude='.cache/*' \
+--exclude='.asdf/*' \
+--include=* $SRC $USER@$HOST:$DEST --chown=soriano:soriano
+
 echo -e '\n\033[0;36mFinish\033[0m\n'
--- scripts/sync-files/restore-files.sh
+++ scripts/sync-files/restore-files.sh
@@ -9,5 +9,5 @@ SRC="/home/soriano/Documentos/restore/"
 clear
 
 echo -e '\n\033[0;36mCopy files...\033[0m\n'
-rsync -azv -e ssh --update --include=* $USER@$HOST:$DEST $SRC --chown=soriano:soriano
+rsync -azv -e ssh --update --exclude='node_modules' --include=* $USER@$HOST:$DEST $SRC --chown=soriano:soriano
 echo -e '\n\033[0;36mFinish\033[0m\n'
