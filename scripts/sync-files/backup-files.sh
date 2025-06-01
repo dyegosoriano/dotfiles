65a3144182a5b84c03c67ff18372e842287220a6
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-06-01 15:21:07 -0300

    refactor: adjust sync-repositories

--- scripts/sync-files/backup-files.sh
+++ scripts/sync-files/backup-files.sh
@@ -13,8 +13,13 @@ echo -e '\n\033[0;36mCopy files...\033[0m\n'
 rsync -azv --delete -e ssh --update \
 --exclude='node_modules' \
 --exclude='.config/*' \
+--exclude='.dotnet/*' \
+--exclude='.docker/*' \
 --exclude='.cache/*' \
 --exclude='.asdf/*' \
+--exclude='.tmux/*' \
+--exclude='.yarn/*' \
+--exclude='.npm/*' \
 --include=* $SRC $USER@$HOST:$DEST --chown=soriano:soriano
 
 echo -e '\n\033[0;36mFinish\033[0m\n'
