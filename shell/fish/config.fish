2dc12c2ac6f2a93546ecbc9b319c6dbcc9a28b59
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-09-16 08:56:24 -0300

    refactor: ajustes e correções no fish shell

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -1,42 +1,41 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 end
 
-starship init fish | source # Inicializando o Starship
-zoxide init fish | source # Inicializando o Zoxide
-fzf --fish | source # Inicializando o FZF
+# Inicializando o Starship, Zoxide e FZF
+starship init fish | source
+zoxide init fish | source
+fzf --fish | source
 
-set -g fish_greeting "" # Removendo a saudação do Fish
+# Removendo a saudação do Fish
+set -g fish_greeting ""
 
-if test "$TERM_PROGRAM" != "WarpTerminal"
-end
+# O bloco abaixo foi removido pois estava vazio
+# if test "$TERM_PROGRAM" != "WarpTerminal"
+# end
 
 if test -d ~/.asdf # ASDF configuration
-  source (brew --prefix asdf)/libexec/asdf.fish
-  set -gx PATH $HOME/.asdf/installs/nodejs/(asdf current nodejs | awk '{print $2}')/.npm/bin $PATH
+  source (brew --prefix asdf)/libexec/asdf.fish
+  set -gx PATH $HOME/.asdf/installs/nodejs/(asdf current nodejs | awk '{print $2}')/.npm/bin $PATH
 end
 
 if test -z $TMUXIFIER # Set/fix Tmuxifier root path if needed.
-  set -gx TMUXIFIER "$HOME/.tmuxifier"
+  set -gx TMUXIFIER "$HOME/.tmuxifier"
 end
 
 if not contains "$TMUXIFIER/bin" $PATH # Add `bin` directroy to `$PATH`.
-  set -gx PATH "$TMUXIFIER/bin" $PATH
+  set -gx PATH "$TMUXIFIER/bin" $PATH
 end
 
-# If `tmuxifier` is available, and `$TMUXIFIER_NO_COMPLETE` is not set, then
-# load Tmuxifier shell completion.
+# Se `tmuxifier` estiver disponível, e `$TMUXIFIER_NO_COMPLETE` não estiver definido, então carregue a conclusão de shell do Tmuxifier.
 if test -n (which tmuxifier); and test -z $TMUXIFIER_NO_COMPLETE
-  if [ (fish --version 2>| awk -F'version ' '{print $2}') = '2.0.0' ]; # fish shell 2.0.0 does not have the source alias
-    . "$TMUXIFIER/completion/tmuxifier.fish"
-  else
-    source "$TMUXIFIER/completion/tmuxifier.fish"
-  end
+  # A verificação da versão do Fish é obsoleta, a linha abaixo foi removida e a sintaxe foi corrigida para usar o comando `source`.
+  source "$TMUXIFIER/completion/tmuxifier.fish"
 end
 
 # Força o carregamento de todas as funções personalizadas
 for file in ~/.dotfiles/shell/fish/functions/*.fish
-    source $file
+    source $file
 end
 
 # Enable the `brew` key bindings
@@ -51,13 +50,8 @@ set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH
 set -q INFOPATH; or set INFOPATH ''
 set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH
 
+export FZF_DEFAULT_OPTS="--walker-skip .git,node_modules,dist --layout reverse --border top --height 40% --style full"
 export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
 export FZF_CTRL_T_OPTS="--preview 'bat -n color=always {}'"
 
-export FZF_DEFAULT_OPTS='
-  --walker-skip .git,node_modules,dist
-  --layout reverse
-  --border top
-  --height 40%
-  --style full
-'
+fish_vi_key_bindings # Ativa vi key bindings
