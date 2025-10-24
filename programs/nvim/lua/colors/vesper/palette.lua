6e4cd6ed75d6a0f1886ffa695a6eb6a8239d446e
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-10-23 21:21:45 -0300

    feat(colors): add vesper color scheme

---
+++ programs/nvim/lua/colors/vesper/palette.lua
@@ -0,0 +1,26 @@
+local M = {
+  background = "#110f18",
+  foreground = "#FFFFFF",
+
+  mono_0 = "#000000",
+  mono_1 = "#101010",
+  mono_2 = "#161616",
+  mono_3 = "#232323",
+  mono_4 = "#282828",
+  mono_5 = "#343434",
+  mono_6 = "#505050",
+
+  secondary = "#FFCFA8",
+  primary = "#99FFE4",
+
+  diff_change = "#FFC799",
+  diff_delete = "#FF8080",
+  diff_add = "#99FFE4",
+
+  error = "#FF8080",
+  warn = "#FFC799",
+  info = "#99FFE4",
+  hint = "#65737E",
+}
+
+return M
