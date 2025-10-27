f743ccad0399d2ba4c40d6c2c663a191e4ede839
Dyego Soriano de Assis <4140145+dyegosoriano@users.noreply.github.com>
2025-10-27 07:40:07 -0300

    feat(lazy): configure lazy.nvim with LazyVim and theme plugins

--- programs/nvim/lua/plugins/treesitter.lua
+++ programs/nvim/lua/plugins/treesitter.lua
@@ -9,18 +9,26 @@ return {
       local config = require("nvim-treesitter.configs")
       config.setup({
         auto_install = false, -- TODO: Identificar do que se trata esse ajuste.
+        highlight = { enable = true },
+        indent = { enable = false },
         ensure_installed = {
+          "markdown_inline",
           "javascript",
           "typescript",
+          "markdown",
+          "python",
+          "query",
+          "regex",
           "scss",
           "json",
           "bash",
+          "yaml",
           "html",
+          "vim",
+          "tsx",
           "css",
           "lua",
         },
-        highlight = { enable = true },
-        indent = { enable = false },
       })
     end
   }
