3834b7c611dd768ac8935e2cd28d1d83091554fe
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-10-31 17:30:26 -0300

    refactor(avante): reorder and add optional dependencies

--- programs/nvim/lua/plugins/avante.lua
+++ programs/nvim/lua/plugins/avante.lua
@@ -91,18 +91,18 @@ return {
   },
   build = 'make',
   dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'stevearc/dressing.nvim',
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
 
-    -- --- The below dependencies are optional,
-    -- 'echasnovski/mini.pick', -- for file_selector provider mini.pick
-    -- 'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
-    -- 'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
-    -- 'ibhagwan/fzf-lua', -- for file_selector provider fzf
-    -- 'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-    -- 'zbirenbaum/copilot.lua', -- for providers='copilot'
+    --- The below dependencies are optional,
+    -- 'nvim-mini/mini.pick',        -- for file_selector provider mini.pick
+    -- 'stevearc/dressing.nvim',     -- for input provider dressing
+    -- 'zbirenbaum/copilot.lua',     -- for providers='copilot'
+    'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+    'nvim-tree/nvim-web-devicons',   -- or echasnovski/mini.icons
+    'folke/snacks.nvim',             -- for input provider snacks
+    'hrsh7th/nvim-cmp',              -- autocompletion for avante commands and mentions
+    'ibhagwan/fzf-lua',              -- for file_selector provider fzf
 
     {
       'HakonHarnes/img-clip.nvim',
