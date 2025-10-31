89057a536e8a820ce92efd58f7fd54a2b5a3ea95
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-10-31 16:37:31 -0300

    refactor(treesitter): migrate config to opts

--- programs/nvim/lua/plugins/treesitter.lua
+++ programs/nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,30 @@
--- https://github.com/nvim-treesitter/nvim-treesitter
--- if true then return {} end
 
 return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    config = function()
-      local config = require('nvim-treesitter.configs')
-      config.setup({
-        auto_install = false, -- TODO: Identificar do que se trata esse ajuste.
-        highlight = { enable = true },
-        indent = { enable = false },
-        ensure_installed = {
-          'markdown_inline',
-          'javascript',
-          'typescript',
-          'markdown',
-          'python',
-          'query',
-          'regex',
-          'scss',
-          'json',
-          'bash',
-          'yaml',
-          'html',
-          'vim',
-          'tsx',
-          'css',
-          'lua',
-        },
-      })
-    end
+    opts = {
+      auto_install = false,
+      highlight = { enable = true },
+      indent = { enable = false },
+      ensure_installed = {
+        'markdown_inline',
+        'javascript',
+        'typescript',
+        'markdown',
+        'python',
+        'query',
+        'regex',
+        'scss',
+        'json',
+        'bash',
+        'yaml',
+        'html',
+        'vim',
+        'tsx',
+        'css',
+        'lua',
+      },
+    },
   }
 }
