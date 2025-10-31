0869fdf07acc5f15cee9334bc687b3b743fd303c
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-10-31 17:44:46 -0300

    feat(telescope): add find_files picker and buffer previewer

--- programs/nvim/lua/plugins/telescope.lua
+++ programs/nvim/lua/plugins/telescope.lua
@@ -3,12 +3,12 @@
 
 return {
   'nvim-telescope/telescope.nvim',
-  enabled = true,
-  tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = {
+    pickers = { find_files = {} },
     defaults = {
       file_ignore_patterns = { 'node_modules/', '%.git/', 'dist/', '%.jpeg', '%.jpg', '%.png', '%.jpg', '%.pdf', '%.ttf' },
+      buffer_previewer_maker = require('telescope.previewers').buffer_previewer_maker,
       layout_strategy = 'horizontal',
     },
   },
