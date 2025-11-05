16d325dc9194a4e65e4056557db67573f97b000c
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-11-04 21:34:38 -0300

    refactor: remove unused 'clsa', 'cls', 'cat', 'b', 'c', 'e' functions

--- shell/fish/functions/default.fish
+++ shell/fish/functions/default.fish
@@ -6,14 +6,6 @@ function ls
   eza --long --group-directories-first --no-permissions --color=always --icons=always --no-time --no-user --grid $argv
 end
 
-function clsa
-  clear && lsa $argv
-end
-
-function cls
-  clear && ls $argv
-end
-
 function rank
   sort | uniq -c | sort -nr | head -n 10 $argv
 end
@@ -24,20 +16,4 @@ end
 
 function history
   history | grep $argv
-end
-
-function cat
-  bat $argv
-end
-
-function b
-  bat $argv
-end
-
-function c
-  clear
-end
-
-function e
-  exit
 end
