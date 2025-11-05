d7d99dae32302e3956c880c9bdef77ba403a9af8
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2025-11-04 21:07:49 -0300

    feat(nvim): enable autoread and file change notifications

--- programs/nvim/lua/config/autocmds.lua
+++ programs/nvim/lua/config/autocmds.lua
@@ -1,8 +1,23 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Habilitar autoread para recarregar arquivos automaticamente
+vim.opt.autoread = true
+
+-- Criar autocmd para verificar mudanças nos arquivos
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  callback = function()
+    if vim.fn.mode() ~= "c" then
+      vim.cmd("checktime")
+    end
+  end,
+})
+
+-- Notificar quando um arquivo for alterado externamente
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+  pattern = "*",
+  callback = function()
+    vim.notify("Arquivo alterado externamente e recarregado!", vim.log.levels.WARN, { timeout = 5000 })
+  end,
+})
