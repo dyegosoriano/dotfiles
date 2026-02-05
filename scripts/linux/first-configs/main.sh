c3e4dd41af68ba2bfdca1ec21e27d1d3c181c164
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2026-02-05 18:40:01 -0300

    feat(scripts): add shell configuration script

---
+++ scripts/linux/first-configs/main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../" &> /dev/null && pwd )"
+
+echo -e '\n\033[0;36mConfiguring shell...\033[0m'
+
+[ -d ~/.config/rmpc ] && mv ~/.config/rmpc ~/.config/rmpc.backup 2>/dev/null; ln -sfn "$SRC/programs/rmpc" ~/.config/rmpc
+[ -d ~/.config/mpd ] && mv ~/.config/mpd ~/.config/mpd.backup 2>/dev/null; ln -sfn "$SRC/programs/mpd" ~/.config/mpd
+[ -d ~/.config/mpv ] && mv ~/.config/mpv ~/.config/mpv.backup 2>/dev/null; ln -sfn "$SRC/programs/mpv" ~/.config/mpv
+
+echo -e '\n\033[0;36mFinished.\033[0m'
+
