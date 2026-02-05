fe2d41321a839350d035771f288c40f2d2fa10b9
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2026-02-05 18:48:25 -0300

    feat(scripts): add symlinks for superfile, yazi, and imv configs

--- scripts/linux/first-configs/main.sh
+++ scripts/linux/first-configs/main.sh
@@ -4,6 +4,10 @@ SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../" &> /dev/null && pwd )"
 
 echo -e '\n\033[0;36mConfiguring shell...\033[0m'
 
+[ -d ~/.config/superfile ] && mv ~/.config/superfile ~/.config/superfile.backup 2>/dev/null; ln -sfn "$SRC/programs/superfile" ~/.config/superfile
+[ -d ~/.config/yazi ] && mv ~/.config/yazi ~/.config/yazi.backup 2>/dev/null; ln -sfn "$SRC/programs/yazi" ~/.config/yazi
+[ -d ~/.config/imv ] && mv ~/.config/imv ~/.config/imv.backup 2>/dev/null; ln -sfn "$SRC/programs/imv" ~/.config/imv
+
 [ -d ~/.config/rmpc ] && mv ~/.config/rmpc ~/.config/rmpc.backup 2>/dev/null; ln -sfn "$SRC/programs/rmpc" ~/.config/rmpc
 [ -d ~/.config/mpd ] && mv ~/.config/mpd ~/.config/mpd.backup 2>/dev/null; ln -sfn "$SRC/programs/mpd" ~/.config/mpd
 [ -d ~/.config/mpv ] && mv ~/.config/mpv ~/.config/mpv.backup 2>/dev/null; ln -sfn "$SRC/programs/mpv" ~/.config/mpv
