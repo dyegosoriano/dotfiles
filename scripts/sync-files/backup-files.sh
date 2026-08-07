1d264add40fd97c3e31f6fc5aa4ee25b52b0f9a2
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2026-08-06 22:22:30 -0300

    feat(backup): limit sync to selected user directories

--- scripts/sync-files/backup-files.sh
+++ scripts/sync-files/backup-files.sh
@@ -4,36 +4,20 @@ DEST="/mnt/SSD-4TB/Backup/XPS-9300/"
 HOST="r2-d2.local"
 USER="soriano"
 
-SRC="/home/soriano"
+SRC="/home/soriano/"
 
 clear
 
 echo -e '\n\033[0;36mCopy files...\033[0m\n'
 
-rsync -azv --delete --force -e ssh --update \
---exclude='node_modules' \
---exclude='.tmuxifier/*' \
---exclude='.dbclient/*' \
---exclude='.windsurf/*' \
---exclude='.codeium/*' \
---exclude='.mcp-hub/*' \
---exclude='.eclipse/*' \
---exclude='.mongodb/*' \
---exclude='.config/*' \
---exclude='.dotnet/*' \
---exclude='.docker/*' \
---exclude='.vscode/*' \
---exclude='.cursor/*' \
---exclude='.cache/*' \
---exclude='.local/*' \
---exclude='.cargo/*' \
---exclude='.asdf/*' \
---exclude='.tmux/*' \
---exclude='.warp/*' \
---exclude='.yarn/*' \
---exclude='.npm/*' \
---exclude='snap/*' \
---exclude='dist/*' \
---include=* $SRC $USER@$HOST:$DEST --chown=soriano:soriano
+rsync -azv --delete --delete-excluded --force -e ssh --update \
+  --exclude='/Documents/**/node_modules/***' \
+  --exclude='/Documents/**/dist/***' \
+  --include='/Documents/***' \
+  --include='/Downloads/***' \
+  --include='/Pictures/***' \
+  --include='/Music/***' \
+  --exclude='*' \
+  "$SRC" "$USER@$HOST:$DEST" --chown=soriano:soriano
 
 echo -e '\n\033[0;36mFinish\033[0m\n'
