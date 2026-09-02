bf97924d0b75c496f594f301b610ac62743c7656
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2026-09-02 14:33:57 -0300

    feat(backup): add ssh key for rsync and new exclude patterns

--- scripts/sync-files/backup-files.sh
+++ scripts/sync-files/backup-files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DEST="/mnt/SSD-4TB/Backup/XPS-9300/"
+SSH_KEY="$HOME/.ssh/xps-9300"
 HOST="r2-d2.local"
 USER="soriano"
 
@@ -10,13 +11,19 @@ clear
 
 echo -e '\n\033[0;36mCopy files...\033[0m\n'
 
-rsync -azv --delete --delete-excluded --force -e ssh --update \
+rsync -azv --delete --delete-excluded --force -e "ssh -i $SSH_KEY" --update \
+  --exclude='/Documents/**/.docker/data/***' \
   --exclude='/Documents/**/node_modules/***' \
+  --exclude='/Documents/**/.angular/***' \
+  --exclude='/Documents/**/.next/***' \
   --exclude='/Documents/**/dist/***' \
   --include='/Documents/***' \
   --include='/Downloads/***' \
+  --include='/.dotfiles/***' \
   --include='/Pictures/***' \
   --include='/Music/***' \
+  --exclude='/.pi/**/node_modules/***' \
+  --include='/.pi/***' \
   --exclude='*' \
   "$SRC" "$USER@$HOST:$DEST" --chown=soriano:soriano
 
