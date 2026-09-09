2d592b8c93c28b6851a5050ef9ad78d2b810f9b9
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2026-09-09 12:04:24 -0300

    refactor(waybar): adjust module order and interval

--- scripts/sync-files/restore-files.sh
+++ scripts/sync-files/restore-files.sh
@@ -3,11 +3,12 @@
 DEST="/mnt/SSD-4TB/Backup/XPS-9300/"
 HOST="r2-d2.local"
 USER="soriano"
+SSH_KEY="$HOME/.ssh/xps-9300"
 
 SRC="/home/soriano/Documentos/restore/"
 
 clear
 
 echo -e '\n\033[0;36mCopy files...\033[0m\n'
-rsync -azv -e ssh --update --exclude='node_modules' --include=* $USER@$HOST:$DEST $SRC --chown=soriano:soriano
+rsync -azv -e "ssh -i $SSH_KEY" --update --exclude='node_modules' --include=* $USER@$HOST:$DEST $SRC --chown=soriano:soriano
 echo -e '\n\033[0;36mFinish\033[0m\n'
